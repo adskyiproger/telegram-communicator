ef0d0abc340b1aaf763f2982094b291861a9a50c
Vadym Mudryi <vadym.mudryi@gmail.com>
2020-01-31 15:39:57 +0000

    working on ...

--- start_here.py
+++ start_here.py
@@ -52,18 +52,57 @@ ACTIVE_USERS={}
 
 logging.info("Bot initialized: "+str(bot.get_me()))
 
+# processUserResponse(): Call upper datamodel and get answer:
+def processUserResponse(update,context,user_msg):
+    REPLY_MARKUP=telegram.ReplyKeyboardRemove()
+    chat_id=update.effective_chat.id
+    # If User questionary is already in process then process user response:
+    if chat_id in ACTIVE_USERS.keys() and ACTIVE_USERS[chat_id].isModel():
+        MESSAGE=ACTIVE_USERS[chat_id].getModel().processQuestion(user_msg)
+        REPLY_MARKUP=ACTIVE_USERS[chat_id].getModel().getMarkup()
+        # If last question in the questionary:
+        if ACTIVE_USERS[chat_id].getModel().getStatus() == 0:
+             saveAnswers(update,context,ACTIVE_USERS[chat_id].getModel().getAnswers())
+             ACTIVE_USERS[chat_id].setModel("NA")
+             MESSAGE+=config['DEFAULT']['BYE_MESSAGE']
+    # If User is in list, but have not started questionary:         
+    # Initialize Questionary
+    elif chat_id in ACTIVE_USERS.keys() and user_msg in categories:
+            ACTIVE_USERS[chat_id].setModel(Model(models[categories.index(user_msg)]))
+            MESSAGE=ACTIVE_USERS[chat_id].getModel().processQuestion(user_msg)
+            
+    # Init new user        
+    # Show greeting message one more time    
+    else:
+        ACTIVE_USERS[chat_id]=User(chat_id,update.effective_chat.first_name)
+        MESSAGE=config['DEFAULT']['GREETING_WORD']+" " \
+                +ACTIVE_USERS[chat_id].getName()+"! "+config['DEFAULT']['WELCOME_MESSAGE']
+        REPLY_MARKUP=start_reply_markup
+
+    context.bot.send_message(chat_id=chat_id, text=MESSAGE, parse_mode=telegram.ParseMode.HTML,reply_markup=REPLY_MARKUP)
+
+
 # Forward Answers to ADMIN_USERs:
 def saveAnswers(update,context,answers):
     user_report=config['DEFAULT']['REPORT_HEADER']+"\n"
-    admin_report=config['DEFAULT']['ADMIN_REPORT_HEADER']+" "+str(update.effective_chat.first_name)+"("+str(update.effective_chat.id)+")\n"
+    admin_report=config['DEFAULT']['ADMIN_REPORT_HEADER'] \
+        +" "+str(update.effective_chat.first_name) \
+        +" ("+str(update.effective_chat.id)+")\n"
     report=""
     for ii in answers:
         report+="<b>"+ii['question']+"</b> "+ii['answer']+"\n"
 
     logging.debug("Report sent to used: "+str(update.effective_chat.id))
-    context.bot.send_message(chat_id=update.effective_chat.id, text=user_report+report, parse_mode=telegram.ParseMode.HTML)
+    
+    context.bot.send_message(
+            chat_id=update.effective_chat.id, 
+            text=user_report+report,
+            parse_mode=telegram.ParseMode.HTML)
+
     for ID in config['DEFAULT']['ADMIN_USER_IDS'].split(","):
-        context.bot.send_message(chat_id=int(ID), text=admin_report+report, parse_mode=telegram.ParseMode.HTML)
+        context.bot.send_message(chat_id=int(ID),
+                text=admin_report+report,
+                parse_mode=telegram.ParseMode.HTML)
 
 # TBD: this function is not working
 def build_menu(buttons,
@@ -83,77 +122,34 @@ def build_menu(buttons,
 # Bot commands:
 # /do_suicide
 def do_suicide(update,context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I do not want to die, but if you really ask me, I will do that. Bye!!!!")
+    context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="I do not want to die, but if you really ask me, I will do that. Bye!!!!")
     updater.stop()
 
 dispatcher.add_handler( CommandHandler('do_suicide', do_suicide) )
 
 # /unknown command processor:
 def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    context.bot.send_message(
+            chat_id=update.effective_chat.id, 
+            text="Sorry, I didn't understand that command.")
 
 dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
 # Process Inline Buttons:
 def button(update, context):
-    chat_id=update.effective_chat.id
     query = update.callback_query
-
-    if chat_id in ACTIVE_USERS.keys() and ACTIVE_USERS[chat_id].isModel():
-        MODEL=ACTIVE_USERS[chat_id].getModel()
-        MESSAGE=ACTIVE_USERS[chat_id].getModel().processQuestion(query.data)
-        REPLY_MARKUP=ACTIVE_USERS[chat_id].getModel().getMarkup()
-        if ACTIVE_USERS[chat_id].getModel().getStatus() == 0:
-             saveAnswers(update,context,ACTIVE_USERS[chat_id].getModel().getAnswers())
-             ACTIVE_USERS[chat_id].setModel("NA")
-             MESSAGE+=config['DEFAULT']['BYE_MESSAGE']
-    elif chat_id in ACTIVE_USERS.keys():
-        if query.data in categories:
-            model=models[categories.index(query.data)]
-            ACTIVE_USERS[chat_id].setModel(Model(model))
-            MESSAGE=ACTIVE_USERS[chat_id].getModel().processQuestion(query.data)
-            REPLY_MARKUP=ACTIVE_USERS[chat_id].getModel().getMarkup()
-        else:
-            MESSAGE=config['DEFAULT']['GREETING_WORD']+" "+ACTIVE_USERS[chat_id].getName()+"! "+config['DEFAULT']['WELCOME_MESSAGE']
-            REPLY_MARKUP=start_reply_markup
-    else:
-        ACTIVE_USERS[chat_id]=User(chat_id,update.effective_chat.first_name)
-        MESSAGE=config['DEFAULT']['GREETING_WORD']+" "+ACTIVE_USERS[chat_id].getName()+"! "+config['DEFAULT']['WELCOME_MESSAGE']
-        REPLY_MARKUP=start_reply_markup
-    query.edit_message_text(text=config['DEFAULT']['SELECTED_OPTION']+" {}".format(query.data))
-    context.bot.send_message(chat_id=chat_id, text=MESSAGE, parse_mode=telegram.ParseMode.HTML,reply_markup=REPLY_MARKUP)
-     
+    query.edit_message_text(
+            text=config['DEFAULT']['SELECTED_OPTION']+" {}"
+                   .format(query.data))
+    processUserResponse(update, context, query.data)
 
 dispatcher.add_handler(CallbackQueryHandler(button))
 
-# Process text responses
+# Process Text responses:
 def echo(update, context):
-    
-    chat_id=update.effective_chat.id
-    REPLY_MARKUP=telegram.ReplyKeyboardRemove()
-    
-    if chat_id in ACTIVE_USERS.keys() and ACTIVE_USERS[chat_id].isModel():
-        MODEL=ACTIVE_USERS[chat_id].getModel()
-        MESSAGE=MODEL.processQuestion(update.message.text)
-        REPLY_MARKUP=ACTIVE_USERS[chat_id].getModel().getMarkup()
-        if ACTIVE_USERS[chat_id].getModel().getStatus() == 0:
-             saveAnswers(update,context,ACTIVE_USERS[chat_id].getModel().getAnswers())
-             ACTIVE_USERS[chat_id].setModel("NA")
-             MESSAGE+=config['DEFAULT']['BYE_MESSAGE']
-    elif chat_id in ACTIVE_USERS.keys():
-        if update.message.text in categories:
-            model=models[categories.index(update.message.text)]
-            ACTIVE_USERS[chat_id].setModel(Model(model))
-            MESSAGE=ACTIVE_USERS[chat_id].getModel().processQuestion(update.message.text)
-        else:
-            MESSAGE=config['DEFAULT']['GREETING_WORD']+" "+ACTIVE_USERS[chat_id].getName()+"! "+config['DEFAULT']['WELCOME_MESSAGE']
-            REPLY_MARKUP=start_reply_markup
-    else:
-        ACTIVE_USERS[chat_id]=User(chat_id,update.effective_chat.first_name)
-        MESSAGE=config['DEFAULT']['GREETING_WORD']+" "+ACTIVE_USERS[chat_id].getName()+"! "+config['DEFAULT']['WELCOME_MESSAGE']
-        REPLY_MARKUP=start_reply_markup
-
-    context.bot.send_message(chat_id=chat_id, text=MESSAGE, parse_mode=telegram.ParseMode.HTML,reply_markup=REPLY_MARKUP)
+    processUserResponse(update, context, update.message.text)
 
 dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
